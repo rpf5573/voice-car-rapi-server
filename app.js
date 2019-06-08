c9bb1bafc70e661349a390468070a4c76b6449da
YOONBYEONGIN <rpf5573@gmail.com>
2019-06-08 10:48:52 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+		http = require('http'),
+		ws = require('ws'),
+		serial_port = require('serialport'),
+		body_parser = require('body-parser');
+
+var serial = new serial_port('/dev/ttyUSB0', {
+	baudRate : 115200
+});
+
+var app = express();
+app.use(body_parser.json());
+
+var server = http.createServer();
+// var sockets = new ws.Server({
+// 	server: server
+// });
+
+// var messages = [];
+
+app.post('/:motor_number/:direction/', function(req, res) {
+	var motor = req.params.motor_number;
+	var dir = req.params.direction;
+	
+	serial.write(motor + "," + dir, function(err) {});
+	//sockets.clients.forEach(function (conn) {
+	//      conn.send("2,"+ motor + "," + dir);
+	//
+});
+
+app.post('/:motor_number/:direction/:speed/', function(req, res) {
+	var motor = req.params.motor_number;
+	var dir = req.params.direction;
+	var speed = req.params.speed;
+	
+	serial.write(motor + "," + dir + "," + speed, function(err) {});
+	//sockets.clients.forEach(function (conn) {
+	//      conn.send("3," + motor + "," + dir +  "," + speed);
+	//});
+
+	res.send("");
+});
+
+server.on('request', app);
+server.listen(8080, function() {
+  console.log("Server Start.");
+});
