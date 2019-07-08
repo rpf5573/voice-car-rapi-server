8adf48b8c8e731def5c471cba00cba57ab56da37
YOONBYEONGIN <rpf5573@gmail.com>
2019-07-08 10:33:16 +0900

    Stop motor after seconds

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express'),
+const express = require('express'),
 		http = require('http'),
 		// ws = require('ws'),
 		serial_port = require('serialport'),
 		body_parser = require('body-parser');
 
 // ttyUSB0
-var serial = new serial_port('/dev/ttyUSB0', {
+const serial = new serial_port('/dev/ttyUSB0', {
 	baudRate : 115200
 });
 
@@ -18,10 +18,10 @@ var serial = new serial_port('/dev/ttyUSB0', {
 // L298n 모터 드라이버와 라즈베리파이를 연결한 배선대로 핀넘버를 설정
 // let l298n = new L298N(17,27,22,null,null,null);
 
-var app = express();
+const app = express();
 app.use(body_parser.json());
 
-var server = http.createServer();
+const server = http.createServer();
 
 // app.get('/smog/:onoff', function(req, res) {
 // 	var onoff = req.params.onoff;
@@ -34,7 +34,7 @@ var server = http.createServer();
 
 app.get('/bottom/:direction', function(req, res) {
 	// stop / forward / backward
-	var dir = req.params.direction;
+	const dir = req.params.direction;
 	serial.write("bottom,"+dir, function(err) {});
 	return res.sendStatus(201);
 });
@@ -45,10 +45,12 @@ app.get('/arm/stop', function(req, res) {
 });
 
 app.get('/:motor_number/:direction/', function(req, res) {
-	var motor = req.params.motor_number;
-	var dir = req.params.direction;
+	const motor = req.params.motor_number;
+	const dir = req.params.direction;
 	
-	serial.write(motor + "," + dir, function(err) {});
+	serial.write(motor + "," + dir, function(err) {
+		if (!err) { stopForSafty(motor); }
+	});
 	//sockets.clients.forEach(function (conn) {
 	//      conn.send("3," + motor + "," + dir +  "," + speed);
 	//});
@@ -57,11 +59,13 @@ app.get('/:motor_number/:direction/', function(req, res) {
 });
 
 app.get('/:motor_number/:direction/:speed/', function(req, res) {
-	var motor = req.params.motor_number;
-	var dir = req.params.direction;
-	var speed = req.params.speed;
+	const motor = req.params.motor_number;
+	const dir = req.params.direction;
+	const speed = req.params.speed;
 	
-	serial.write(motor + "," + dir + "," + speed, function(err) {});
+	serial.write(motor + "," + dir + "," + speed, function(err) {
+		if (!err) { stopForSafty(motor); }
+	});
 	//sockets.clients.forEach(function (conn) {
 	//      conn.send("3," + motor + "," + dir +  "," + speed);
 	//});
@@ -71,9 +75,27 @@ app.get('/:motor_number/:direction/:speed/', function(req, res) {
 
 app.get('/', function(req, res) {
 	res.sendStatus('201');
-})
+});
 
 server.on('request', app);
 server.listen(8080, function() {
   console.log("Server Start.");
 });
+
+function stopForSafty(motor) {
+	if ( motor == 'motor-2' || motor == 'motor-5' || motor == 'motor-6' ) {
+		let stopTime = 5000;
+		if ( motor == 'motor-2' ) {
+			stopTime = 8000;
+		}
+		else if (motor == 'motor-5') {
+			stopTime = 5000;
+		}
+		else if (motor == 'motor-6') {
+			stopTime = 3000;
+		}
+		setTimeout(()=>{
+			serial.write(motor + ",stop");
+		}, stopTime);
+	}
+}
