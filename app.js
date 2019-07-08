c26cf802df3e6af94a8e9fcbac4a839534a4f72a
YOONBYEONGIN <rpf5573@gmail.com>
2019-07-08 14:21:46 +0900

    Delete unused code

--- app.js
+++ app.js
@@ -15,28 +15,10 @@ let currentMovingMotorInfo = {
 };
 let timerForStop = false;
 
-// pigpio
-// const Gpio = require('pigpio').Gpio;
-// const LOW = 0;
-// const HIGH = 1;
-// const L298N = require('./motor/l298n.js');
-//bcm code
-// L298n 모터 드라이버와 라즈베리파이를 연결한 배선대로 핀넘버를 설정
-// let l298n = new L298N(17,27,22,null,null,null);
-
 const app = express();
 app.use(body_parser.json());
 
 const server = http.createServer();
-
-// app.get('/smog/:onoff', function(req, res) {
-// 	var onoff = req.params.onoff;
-// 	if ( onoff == 'on' ) {
-// 		l298n.forward();
-// 	} else {
-// 		l298n.stop();
-// 	}
-// });
 
 app.get('/bottom/:direction', function(req, res) {
 	// stop / forward / backward
@@ -57,9 +39,6 @@ app.get('/:motor_number/:direction/', function(req, res) {
 	serial.write(motor + "," + dir, function(err) {
 		if (!err && dir != 'stop') { stopForSafty(motor, dir); }
 	});
-	//sockets.clients.forEach(function (conn) {
-	//      conn.send("3," + motor + "," + dir +  "," + speed);
-	//});
 
 	return res.sendStatus(201);
 });
@@ -72,9 +51,6 @@ app.get('/:motor_number/:direction/:speed/', function(req, res) {
 	serial.write(motor + "," + dir + "," + speed, function(err) {
 		if (!err && dir != 'stop') { stopForSafty(motor, dir); }
 	});
-	//sockets.clients.forEach(function (conn) {
-	//      conn.send("3," + motor + "," + dir +  "," + speed);
-	//});
 
 	return res.sendStatus(201);
 });
