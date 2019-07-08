052ce82a160e3b793ff6b6bbbd03c92f008b4bcc
YOONBYEONGIN <rpf5573@gmail.com>
2019-07-08 11:16:39 +0900

    Minor change

--- app.js
+++ app.js
@@ -86,13 +86,13 @@ function stopForSafty(motor) {
 	if ( motor == 'motor-2' || motor == 'motor-5' || motor == 'motor-6' ) {
 		let stopTime = 1500;
 		if ( motor == 'motor-2' ) {
-			// stopTime = 8000;
+			stopTime = 8000;
 		}
 		else if (motor == 'motor-5') {
-			// stopTime = 5000;
+			stopTime = 5000;
 		}
 		else if (motor == 'motor-6') {
-			// stopTime = 3000;
+			stopTime = 3000;
 		}
 		setTimeout(()=>{
 			serial.write(motor + ",stop");
