55291d70e361a2ad92f5a9928b9353c7c5e65d04
Yoon <rpf5573@gmail.com>
2019-10-10 21:59:12 +0900

    command system update

--- app.js
+++ app.js
@@ -20,79 +20,64 @@ app.use(body_parser.json());
 
 const server = http.createServer();
 
-app.get('/bottom/:direction/:speed', function(req, res) {
-	// stop / forward / backward
-	const dir = req.params.direction;
-	const speed = req.params.speed;
+var globalTimer = {
+  // 손
+  10: false,
+  11: false,
+  12: false,
+  
+  // 팔
+  20: false,
+  21: false,
+  22: false,
+  23: false,
+  24: false,
 
-	var command = "bottom,"+dir+","+speed;
-	serial.write(command, function(err) {
-		if ( err ) { console.error(err); }
+  // 몸체
+  30: false,
+  31: false,
+  32: false,
+
+  // 다리
+  40: false,
+  41: false,
+  42: false,
+  43: false,
+  44: false,
+  45: false,
+}
+
+app.get('/:code', (req, res) => {
+	const code = req.params.code;
+	serial.write(code, function(err) {
+		if ( err ) {
+			console.error(err); 
+		} else {
+			// stop 명령은 10, 20, 30, 40 ... 이런식이다.
+			let isStopCode = (code%10 == 0) ? true : false;
+			let partCode = Math.floor(code/10);
+			if ( ! isStopCode && (partCode == 1 || partCode == 2) ) {
+				stopForSafty(code);
+			}
+		}
 	});
-
 	return res.sendStatus(201);
 });
 
-app.get('/arm/stop', function(req, res) {
-	serial.write("arm,stop", function(err) {});
-	return res.sendStatus(201);
-});
+function stopForSafty(code) {
+	// 이렇게 멈추는게 맞기는 한데,,, 만약에 3초안에 같은 모터 동작 명령이 들어오면,,,갑자기 멈출 수는 없잖아!!!
+	if ( globalTimer[code] ) {
+		clearTimeout(globalTimer[code]);
+	}
 
-app.get('/:motor_number/:direction/:speed/', function(req, res) {
-	const motor = req.params.motor_number;
-	const dir = req.params.direction;
-	const speed = req.params.speed;
-
-	var command = motor + "," + dir + "," + speed;
-	console.log("command : ", command);
-	serial.write(command, function(err) {
-		console.log("err : ", err);
-		if (!err && dir != 'stop') { stopForSafty(motor, dir); }
-	});
-
-	return res.sendStatus(201);
-});
-
-app.get('/:motor_number/:direction/', function(req, res) {
-	const motor = req.params.motor_number;
-	const dir = req.params.direction;
-
-	serial.write(motor + "," + dir, function(err) {
-		if (!err && dir != 'stop') { stopForSafty(motor, dir); }
-	});
-
-	return res.sendStatus(201);
-});
-
-app.get('/', function(req, res) {
-	res.sendStatus('201');
-});
+	globalTimer[code] = setTimeout(() => {
+		let stopCode = Math.floor(code/10) * 10;
+		serial.write(stopCode);
+		globalTimer[code] = false;
+	}, 6000);
+}
 
 server.on('request', app);
 server.listen(8080, function() {
   console.log("Server Start.");
 });
-
-function stopForSafty(motor, dir) {
-	if ( motor == 'motor-2' || motor == 'motor-5' || motor == 'motor-6' ) {
-		if ( motor == currentMovingMotorInfo.motor && dir != currentMovingMotorInfo.direction && timerForStop ) {
-			clearTimeout(timerForStop);
-		}
-		let stopTime = 1500;
-		if ( motor == 'motor-2' ) {
-			stopTime = 8000;
-		}
-		else if (motor == 'motor-5') {
-			stopTime = 5000;
-		}
-		else if (motor == 'motor-6') {
-			stopTime = 3000;
-		}
-		timerForStop = setTimeout(()=>{
-			serial.write(motor + ",stop");
-		}, stopTime);
-		
-		currentMovingMotorInfo.motor = motor;
-		currentMovingMotorInfo.direction = dir;
-	}
-}
